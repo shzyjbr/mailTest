ca0bb9178c2e5dfaa2902b2d57656925b06d8438
zzk31 <417160807@qq.com>
2018-08-06 22:17:52 +0800

    commit all files

---
+++ src/main/java/Main.java
@@ -0,0 +1,61 @@
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+import com.sun.mail.util.MailSSLSocketFactory;
+public class Main {
+    public static void main(String [] args) throws GeneralSecurityException
+    {
+        // 收件人电子邮箱
+        String to = "417160807@qq.com";
+
+        // 发件人电子邮箱
+        String from = "15807731669@163.com";
+
+        // 指定发送邮件的主机为 smtp.qq.com
+        String host = "smtp.163.com";  //QQ 邮件服务器
+
+        // 获取系统属性
+        Properties properties = System.getProperties();
+
+        // 设置邮件服务器
+        properties.setProperty("mail.smtp.host", host);
+
+        properties.put("mail.smtp.auth", "true");
+        MailSSLSocketFactory sf = new MailSSLSocketFactory();
+        sf.setTrustAllHosts(true);
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.ssl.socketFactory", sf);
+        // 获取默认session对象
+        Session session = Session.getDefaultInstance(properties,new Authenticator(){
+            public PasswordAuthentication getPasswordAuthentication()
+            {
+                return new PasswordAuthentication("15807731669@163.com", "0754zzk"); //发件人邮件用户名、密码
+            }
+        });
+
+        try{
+            // 创建默认的 MimeMessage 对象
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: 头部头字段
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: 头部头字段
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: 头部头字段
+            message.setSubject("This is the Subject Line!");
+
+            // 设置消息体
+            message.setText("This is actual message");
+
+            // 发送消息
+            Transport.send(message);
+            System.out.println("Sent message successfully....from runoob.com");
+        }catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
